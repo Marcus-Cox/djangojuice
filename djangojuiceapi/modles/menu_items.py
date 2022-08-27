737719fefe5feef92b59b07dbfdb290d869c4f88
Marcus-Cox <marcus.cox92@gmail.com>
2022-08-27 06:58:02 -0500

    start of modles

---
+++ djangojuiceapi/modles/menu_items.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class MenuItem(models.Model):
+    
+    title = models.CharField(max_length=55)
+    description = models.CharField(max_length=255)
+    price =models.DecimalField(max_digits=5, decimal_places=2)
+    type = models.CharField(max_length=55)
+    
+    
